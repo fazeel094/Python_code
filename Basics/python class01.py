0d697422f51ee1bc06bf554b9a97dfb177e5331a
fazeel <fazeelfz6@gmail.com>
2021-10-20 19:37:33 +0530

    class01

---
+++ Basics/python class01.py
@@ -0,0 +1,94 @@
+Features of Python:
+    ->open source
+    ->interpreted
+    ->dynamically typed
+    ->extensive libs
+    ->easy syntaxed
+    ->strict indentations
+    ->cross platform
+
+open source:easily available to download no need of any license
+python.org--->>3.7
+interpreted:line by line
+
+Compiled vs interpreted :
+     compiled -> C, Java
+
+     ----
+     ----
+     --7-     --->> compile -> compile file --> run--> error line 7 and 19.
+     ----
+     ----
+     --19-
+     100
+
+
+
+     intepreted  -> python
+
+     ----
+     ----
+     --7-       --->> interpret -> output till 6,error at 7 -->> output till 18,error at 19
+                            interpret -->> 100% output.
+     ----
+     ----
+     --29
+     ----
+     ----
+     100
+     
+Dynamically typed:
+    There is no need to type datatypes to declare/change a value or variable
+    10 --> dynamically typing -> py
+    int 10 --> C, Java , C++
+
+
+Easy Syntaxed:
+    grammer of cde
+    easy
+    ; {} ---> C controller
+    :  --> py controller
+
+
+Strict Intendations:
+    use of spaces
+    error
+
+
+Cross platform:
+    single code --> multiple OS
+
+
+
+Keywords:  Pre defined words,libs,immutable.
+--------
+py2 has 31 keywords
+py3.3 has 33
+py3.7 has 35
+
+
+>>>import keyword
+>>>keyword.kwlist
+['False', 'None', 'True', '__peg_parser__', 'and', 'as', 'assert', 'async',
+ 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except',
+ 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+ 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with','yield']
+    
+Identifiers:
+    user defined words
+    used for naming components
+    del add modify
+    rules:
+        start lowercase
+        start uppercase(not recommended)
+        cannot start with number --xx
+        cannot star with symbol--xx
+        _ -- > special case
+        use number
+        cannot use symbol--xx
+
+ a, name, Age, _user, __password, mobile2 -->> valid
+ 2mobile, ~age, first$name -->> invalid
+ 
+        
+     
